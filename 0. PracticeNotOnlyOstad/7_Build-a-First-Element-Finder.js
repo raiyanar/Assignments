34b586037bec2309b845313080e251db3cf01243
Adib Raiyan <adibraiyanruhsaf@gmail.com>
2026-08-14 22:34:19 +0600

    first element finder

---
+++ 0. PracticeNotOnlyOstad/7_Build-a-First-Element-Finder.js
@@ -0,0 +1,13 @@
+function findElement(arr, func) {
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i]) === true) {
+      return arr[i];
+    }
+  }
+}
+
+console.log(
+  findElement([1, 3, 5, 8, 9, 10], function (num) {
+    return num % 2 === 0;
+  }),
+);
