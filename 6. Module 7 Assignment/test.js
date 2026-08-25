e90404b8ffbc5dc8873aaf5c0ebc55c93c86815a
Adib Raiyan <adibraiyanruhsaf@gmail.com>
2026-08-25 20:24:06 +0600

    delete: unwanted test.js file deleted from module 7 assignment folder

--- 6. Module 7 Assignment/test.js
+++
@@ -1,8 +0,0 @@
-function greet(name, callback) {
-  callback(name);
-}
-greet("Amit", callbackFunction);
-
-function callbackFunction(name) {
-  console.log(`Hello ${name}\nWelcome to JavaScript`);
-}
